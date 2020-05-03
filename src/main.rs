b65316067669e001b8d46b12f7bb182cce5b008d
Tatsunori Uchino <tats.u@live.jp>
2020-05-03 18:47:33 +0900

    Add Add subcommand to make manifest support UTF-8

--- src/main.rs
+++ src/main.rs
@@ -146,14 +146,72 @@ fn main() {
                 error_and_exit!(err);
             }
         }
+    } else if let Some(ref matches) = matches.subcommand_matches("apply-manifest") {
+        let input_path_str = matches.value_of_os("in").unwrap();
+        let input_path = Path::new(&input_path_str);
+        let output_path_option = matches.value_of_os("out");
+        let overwriting_allowed = matches.is_present("force");
+        if !input_path.is_file() {
+            error_and_exit!(format!(
+                "The input file {} is not a file.",
+                input_path_str.to_string_lossy().green()
+            ));
+        }
+        let output_path_str_ref = match output_path_option {
+            Some(ref output_path_str) => &output_path_str,
+            None => {
+                // Overwriting mode (in = out)
+                &input_path_str
+            }
+        };
+        let output_path = Path::new(output_path_str_ref);
+        if output_path.exists() && !output_path.is_file() {
+            error_and_exit!(format!(
+                "It is not allowed to pass a path {} for non-file as output.",
+                output_path_str_ref.to_string_lossy().green()
+            ));
+        }
+        if !overwriting_allowed && output_path.exists()
+        /* && output_path.is_file() */ // already implied
+        {
+            error_and_exit!(format!(
+                "Overwriting is allowed only when {} option is given.",
+                "-f".green()
+            ));
+        }
+        let working_dir = tempdir().unwrap();
+        let u8manifest_path = working_dir.path().join("utf8.manifest");
+        create_manifest_file(&u8manifest_path).unwrap();
+        let mut apply_manifest_result = Command::new("mt")
+            .args(&[
+                "-nologo",
+                "-manifest",
+                &input_path.to_string_lossy(),
+                &u8manifest_path.to_string_lossy(),
+                &format!("-out:{}", &output_path.to_string_lossy()),
+            ])
+            .spawn();
+        match apply_manifest_result {
+            Ok(ref mut psinfo) => {
+                psinfo.wait().unwrap();
+                println!(
+                    "{}{}",
+                    "Succeeded to generate UTF-8 manifest: ",
+                    output_path.to_string_lossy().green()
+                )
+            }
+            Err(ref err) => {
+                error_and_exit!(err);
+            }
+        }
     } else if let Some(ref matches) = matches.subcommand_matches("manifest") {
         let outputpath = Path::new(matches.value_of_os("output").unwrap());
         let overwriting_allowed = matches.is_present("force");
         if outputpath.is_dir() {
             error_and_exit!(format!(
                 "{} is a directory.  Pass a different path",
-                outputpath.to_string_lossy().green())
-            );
+                outputpath.to_string_lossy().green()
+            ));
         }
         if outputpath.is_file() && !overwriting_allowed {
             error_and_exit!(format!(
